f05427f944721572f121df6dc774a0e7c88a4583
Zoe Ang <zwAng@mcm.edu.ph>
2025-03-09 00:06:43 +0800

    Add files via upload

---
+++ MODIFY.java
@@ -0,0 +1,9 @@
+public class MODIFY {
+    public static void main(String[] args) {
+     int[] numbers = {8, 17, 24, 32, 40};
+     System.out.println("SECOND ELEMENT: " + numbers[1]);
+     
+     numbers[1] = 16;
+     System.out.println("MODIFIED SECOND ELEMENT: " + numbers[1]);
+    }
+}
